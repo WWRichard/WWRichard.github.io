353395102a9c7ba3643fb96f25ead6a1a118e485
WWRichard <71507408+WWRichard@users.noreply.github.com>
2024-09-08 13:47:17 +0800

    Update hifini.js

--- music/hifini.js
+++ music/hifini.js
@@ -126,7 +126,7 @@ module.exports = {
     platform: "hifini", // 插件名
     version: "0.0.1", // 版本号
     author: "Ri",
-
+    srcUrl: "https://wwrichard.github.io/music/hifini.js",
     cacheControl: "no-store", // 我们可以直接解析出musicItem的结构，因此选取no-store就好了，当然也可以不写这个字段
     async search(query, page, type) {
         //const uu = encodeChineseURL(query)
