1fa406f0506ba738a4e7c5c59d43c96604852ddd
WWRichard <71507408+WWRichard@users.noreply.github.com>
2024-09-08 13:52:56 +0800

    Update freemp3cn.js

--- music/freemp3cn.js
+++ music/freemp3cn.js
@@ -39,6 +39,7 @@ module.exports = {
     platform: "FreeMp3Cn", // 插件名
     version: "0.0.1", // 版本号
     author: "Ri",
+    srcUrl: "https://wwrichard.github.io/music/freemp3cn.js",
     cacheControl: "no-store", // 我们可以直接解析出musicItem的结构，因此选取no-store就好了，当然也可以不写这个字段
     async search(query, page, type) {
         if (type === "music") {
